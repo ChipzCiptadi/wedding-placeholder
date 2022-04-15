98b345d4dfcfcee07d1d90ee32af153924d6244f
Ciptadi <chipz.ciptadi@gmail.com>
2022-04-15 22:19:24 +0700

    hide kidung and zoom before dday

---
+++ src/index.ori.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const $ = (id) => document.getElementById(id);
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+const weddingURL = "#";
+const dear = params.dear ?? "guest";
+
+$("invitation").href = weddingURL + "?u=" + dear;
+
+let today = new Date();
+const dday = 1651683600000; // 2022-05-05 00:00:00.000
+
+if (today.getTime() < dday) {
+    $("kidung").style.display = "none";
+    $("zoom").style.display = "none";
+}
